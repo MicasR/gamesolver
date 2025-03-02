5e68f658c5470f58fe4548ac55a69068f4badfad
dmicas <65303708+dmicas@users.noreply.github.com>
2025-03-02 23:22:01 +0200

    added default values to model

--- games/water_color_sorting/schemas.py
+++ games/water_color_sorting/schemas.py
@@ -3,14 +3,14 @@ from pydantic import BaseModel, field_validator
 
 
 class PositionInScreen(BaseModel):
-    x: int
-    y: int
+    x: int = 0
+    y: int = 0
 
 
 class RgbColorCode(BaseModel):
-    r: int
-    g: int
-    b: int
+    r: int = 0
+    g: int = 0
+    b: int = 0
 
     @field_validator('r','g','b')
     @classmethod
@@ -22,17 +22,18 @@ class RgbColorCode(BaseModel):
 
 class Color(BaseModel):
     name: str = "unknown"
-    rgb_code: RgbColorCode
-    position: PositionInScreen
+    rgb_code: RgbColorCode = RgbColorCode()
+    position: PositionInScreen = PositionInScreen()
 
 
 class Tube(BaseModel):
     """Model representing a tube in the water sorting game"""
+
     position: PositionInScreen
-    color_1: Color
-    color_2: Color
-    color_3: Color
-    color_4: Color
+    color_1: Color = Color()
+    color_2: Color = Color()
+    color_3: Color = Color()
+    color_4: Color = Color()
 
 
 class GameState(BaseModel):
@@ -40,21 +41,4 @@ class GameState(BaseModel):
     tubes: dict[str, Tube]
 
 
-# Example of a game state
-# game_state = GameState(tubes={
-#     "tube_1": Tube(
-#         position=PositionInScreen(x=0, y=0),
-#         color_1=Color(rgb_code=RgbColorCode(r=0, g=0, b=0), position=PositionInScreen(x=0, y=0)),
-#         color_2=Color(rgb_code=RgbColorCode(r=0, g=0, b=0), position=PositionInScreen(x=0, y=0)),
-#         color_3=Color(rgb_code=RgbColorCode(r=0, g=0, b=0), position=PositionInScreen(x=0, y=0)),
-#         color_4=Color(rgb_code=RgbColorCode(r=0, g=0, b=0), position=PositionInScreen(x=0, y=0)),
-#     ),
-#     "tube_2": Tube(
-#         position=PositionInScreen(x=1, y=0),
-#         color_1=Color(rgb_code=RgbColorCode(r=0, g=0, b=0), position=PositionInScreen(x=1, y=0)),
-#         color_2=Color(rgb_code=RgbColorCode(r=0, g=0, b=0), position=PositionInScreen(x=1, y=0)),
-#         color_3=Color(rgb_code=RgbColorCode(r=0, g=0, b=0), position=PositionInScreen(x=1, y=0)),
-#         color_4=Color(rgb_code=RgbColorCode(r=0, g=0, b=0), position=PositionInScreen(x=1, y=0)),
-#     )
-# })
 
